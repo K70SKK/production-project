191f3c1bfb9ddbbe3b0c8cfb292094d2d5a94d74
c3620284 <s.ahmed2629@student.leedsbeckett.ac.uk>
2024-03-13 23:02:32 +0000

    step 2 textbox added with steps

--- resources/views/step2.blade.php
+++ resources/views/step2.blade.php
@@ -1,1 +1,14 @@
-ddd
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Step 2: Secure the Scene') }}
+        </h2>
+    </x-slot>
+
+    
+    <textarea class="w-full h-40 border border-gray-300 rounded-md p-2 mt-4 focus:outline-none focus:ring focus:border-blue-300" placeholder="Make sure that the only people at the crime scene have the authority to be there.
+Limit the amount of people that have acsess to the scence to avoid breakcing the intherity of evidnce  
+Make sure that nothing gets taken in or outside of the sence
+Take notes of any posibble evidnce you can see " rows="4" cols="50"></textarea>
+
+</x-app-layout>
