a016e8e9e9387679d489325c41514d5c592efdcc
c3620284 <s.ahmed2629@student.leedsbeckett.ac.uk>
2024-03-29 02:42:48 +0000

    values chnaged

--- resources/views/1step.blade.php
+++ resources/views/1step.blade.php
@@ -14,12 +14,12 @@
                         @csrf
                         <div class="mb-4">
                             <label for="first_responder_name" class="block text-sm font-medium text-gray-700">First Responder Name</label>
-                            <input type="text" name="first_responder_name" id="first_responder_name" autocomplete="given-name" value="Sikandar Ahmed" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                            <input type="text" name="first_responder_name" id="first_responder_name" autocomplete="given-name" value="<First Responder Name>" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
                         </div>
 
                         <div class="mb-4">
                             <label for="company_data" class="block text-sm font-medium text-gray-700">Company Data</label>
-                            <input type="text" name="company_data" id="company_data" autocomplete="organization" value="LBU" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                            <input type="text" name="company_data" id="company_data" autocomplete="organization" value="<Company Name>" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
                         </div>
 
                         <div class="mb-4">
